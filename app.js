bca4f6897daf1cd8fe7f6744e677285758393bb2
Chris Polis <cmpolis@gmail.com>
2011-06-06 10:25:35 -0700

    refactored app.js

--- app.js
+++ app.js
@@ -2,44 +2,24 @@
 var nouns = ['speakers', 'oranges', 'sweaters', 'lamps', 'writing a blog', 'semicolons'];
 var adjectives = ['loud', 'fun', 'nerdy'];
 
-var express = require('express');
-var io = require('socket.io');
+// Dependencies
+var express = require('express'),
+    io = require('socket.io'),
+    ar = require('couch-ar'),
+    jade = require('jade');
 
 // Connect to database and load models from /models dir
-var ar = require('couch-ar')
 ar.init({
   dbName: 'apples',
   root: __dirname + '/models'
 }, function(db){ 
+ 
   // Ensure enough time for models to load
   setTimeout(1000);
 });
 
 // Setup express
 var app = express.createServer(express.bodyParser());
-
-// Game logic functions
-
-// Assign user 7 noun cards
-var deal_nouns = function(user_id, client) {
-  for(var ndx = 0; ndx < 7; ndx++) {
-    var word = nouns[Math.floor(Math.random()*nouns.length)];
-    var new_noun = ar.Noun.create({word: word, user_id: user_id});
-    
-    new_noun.save(function(err, db_res){});
-    
-    // Notify on client side
-    client.send({
-      event: 'new_card',
-      data: new_noun.word
-    });
-  }
-}
-
-//Setup game
-
-
-require('jade');
 app.set('view engine', 'jade');
 app.set('view options', {layout: false});
 app.use(express.logger());
@@ -48,15 +28,34 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-
 app.listen(3001);
+
+// Assign user a noun card
+var deal_noun = function(user_id, client) {
+  var word = nouns[Math.floor(Math.random()*nouns.length)];
+  var new_noun = ar.Noun.create({word: word, user_id: user_id});
+   
+  new_noun.save(function(err, db_res){
+    
+    // Notify client of each card after save
+    client.send({
+      event: 'new_card',
+      data: new_noun.word
+    });
+  });
+
+};
+
+//Assign user 7 cards
+var deal_nouns = function(user_id, client) {
+  for(var ndx = 0; ndx < 7; ndx++) {
+    deal_noun(user_id, client);
+  }
+};
 
 var socket = io.listen(app);
 
 socket.on('connection', function(client){
-  console.log('user has connected');
-  console.log(client.ip);
-
   user = ar.User.create({name: client.sessionID});
   user.save(function(err, db_res){
     deal_nouns(user.id, client);
